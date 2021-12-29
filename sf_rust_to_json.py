bfb667e8d2b6bf6f8a62382b6e3c4bc21a7c85f3
MegaIng <cornelius@krupp.hamburg>
2021-12-29 11:50:26 +0100

    Added pydot output and copied over code to transform Starficz's output

---
+++ sf_rust_to_json.py
@@ -0,0 +1,121 @@
+import json
+
+import lark
+from lark import Transformer
+
+parser = lark.Lark("""
+solutions: "{" (solution ("," solution)*)? "}"
+solution: "{" (gate ("," gate)*)? "}" | "[" (gate ("," gate)*)? "]"  
+gate: "Gate" ":"? "{" tt "," delay "," gate_type "}"
+tt: "truth_table" ":" INT
+delay: "delay" ":" INT
+gate_type: "gate_type" ":" CNAME ("(" (INT ("," INT)*)? ")")?
+
+%import common.INT
+%import common.CNAME
+%import common.WS
+
+%ignore WS
+""", parser='lalr', start=['solutions', 'solution'])
+
+
+class ToJson(Transformer):
+    def __default__(self, data, children, meta):
+        raise NotImplementedError(data, children, meta)
+
+    def gate_type(self, children):
+        return children[0].value, tuple(int(a.value) for a in children[1:])
+
+    def tt(self, children):
+        return int(children[0])
+
+    def delay(self, children):
+        return int(children[0])
+
+    def gate(self, children):
+        tt, delay, (gt, args) = children
+        return {"truth_table": tt, "delay": delay, "gate_type": gt, "args": args}
+
+    def solution(self, children):
+        return children
+
+    def solutions(self, children):
+        return children
+
+
+# tree = parser.parse(open("FA_6_4.txt").read())
+tree = parser.parse("""
+[Gate { truth_table: 15, delay: 0, gate_type: InputGate }, 
+Gate { truth_table: 51, delay: 0, gate_type: InputGate }, 
+Gate { truth_table: 85, delay: 0, gate_type: InputGate }, 
+Gate { truth_table: 65514, delay: 2, gate_type: Nand(3, 4) }, 
+Gate { truth_table: 65429, delay: 4, gate_type: Nand(7, 8) }, 
+Gate { truth_table: 65532, delay: 2, gate_type: Nand(1, 2) }, 
+Gate { truth_table: 23, delay: 4, gate_type: Nand(8, 32) }, 
+Gate { truth_table: 105, delay: 6, gate_type: Nand(40, 80) }, 
+Gate { truth_table: 105, delay: 6, gate_type: Or(128) }, 
+Gate { truth_table: 23, delay: 4, gate_type: Or(64) }]
+""", start="solution")
+
+data = ToJson().transform(tree)
+
+
+inputs = [0, 0b0000_1111, 0b0011_0011, 0b0101_0101, 0b1111_1111]
+outputs = [0b0110_1001, 0b0001_0111]
+
+
+def std_from_bitfield(network, known_inputs, outputs):
+    indexed_gates = []
+    out_gates = []
+
+    for gate in network:
+        match gate:
+            case {"gate_type": "Nand" | "InputGate", "truth_table": tt}:
+                indexed_gates.append(tt & 0xFF)
+
+    known_ors = set()
+
+    def get_or(bitfield):
+        needed_tts = []
+        i = 0
+        result = 0
+        while bitfield:
+            if bitfield & 1:
+                needed_tts.append(indexed_gates[i])
+                result |= indexed_gates[i]
+            bitfield //= 2
+            i += 1
+        needed_tts = tuple(sorted(needed_tts))
+        if len(needed_tts) > 1 and needed_tts not in known_ors:
+            out_gates.append({
+                "gate_type": "or",
+                "truth_table": result,
+                "sources": needed_tts
+            })
+            known_ors.add(needed_tts)
+        return result
+
+    for gate in network:
+        match gate:
+            case {"gate_type": "Nand", "truth_table": tt, "delay": delay, "args": [left, right]}:
+                out_gates.append({
+                    "gate_type": "nand",
+                    "truth_table": tt,
+                    "delay": delay,
+                    "sources": [get_or(left), get_or(right)]
+                })
+            case {"gate_type": "Or", "truth_table": tt, "delay": delay, "args": [value]}:
+                get_or(value)
+            case {"gate_type": "InputGate", "truth_table": tt}:
+                assert tt in known_inputs, (tt, bin(tt))
+            case other:
+                raise NotImplementedError(other)
+    return {
+        "inputs": known_inputs,
+        "outputs": outputs,
+        "gates": out_gates
+    }
+
+
+s = json.dumps(std_from_bitfield(data, inputs, outputs), indent=2)
+print(s)
--- validate_solution.py
+++ validate_solution.py
@@ -1,4 +1,5 @@
 import json
+from collections import deque
 from dataclasses import dataclass
 from functools import cached_property, reduce
 from operator import or_
@@ -35,9 +36,18 @@ class Validator:
                     f"Truth Table {self.f(tt)}, which hasn't been produced at this point.\n"
                     f"Available at this point: {', '.join(map(self.f, tts))}"
                 )
+
+        def check_tt(tt: int) -> int:
+            if tt < 0 or int(tt) != tt or tt > self.mask:
+                raise ValidationError(f"Encountered Invalid Truth Table {tt!r}")
+            return int(tt)
+
         for gate in gates:
             match gate:
                 case {"gate_type": "nand", "truth_table": expected, "sources": [left, right]}:
+                    expected = check_tt(expected)
+                    left = check_tt(left)
+                    right = check_tt(right)
                     if (v := (~(left & right)) & self.mask) != expected:
                         raise ValidationError(
                             f"Nand Gate failed to validated. Claims to produce {self.f(expected)} from "
@@ -48,6 +58,8 @@ class Validator:
                     require("Nand Gate", expected, right)
                     tts.add(expected)
                 case {"gate_type": "or", "truth_table": expected, "sources": [*sources]}:
+                    expected = check_tt(expected)
+                    *sources, = map(check_tt, sources)
                     if (v := reduce(or_, sources)) & self.mask != expected:
                         expr = " OR ".join(map(self.f, sources))
                         raise ValidationError(
@@ -59,12 +71,82 @@ class Validator:
                         require("Or Gate", expected, source)
                     tts.add(expected)
                 case other:
-                    raise ValidationError(f"Unknown Gate structure: {other}")
+                    raise ValidationError(f"Unknown Gate type: {other}")
+
+    def sort(self, gates, inputs):
+        """
+
+        :param gates:
+        :param inputs:
+        :return:
+        """
+        all_available = {*inputs, *(gate["truth_table"] for gate in gates)}
+        remaining = deque(gates)
+        done = set(inputs)
+        out = []
+        seen_since_insert = set()
+        while remaining:
+            current = remaining.popleft()
+            if current["truth_table"] in seen_since_insert:
+                raise ValidationError(f"Detected Circle involving these truth table gates: {seen_since_insert}")
+            if any((witness := source) not in all_available for source in current["sources"]):
+                raise ValidationError(f"Required Truth table {witness} appears to be unreachable")
+            if all(source in done for source in current["sources"]):
+                seen_since_insert.clear()
+                out.append(current)
+                done.add(current["truth_table"])
+            else:
+                seen_since_insert.add(current["truth_table"])
+                remaining.append(current)
+
+        return out
+
+    def to_pydot(self, tcsol):
+        import pydot
+
+        dot = pydot.Dot(rankdir="LR")
+
+        for inp in tcsol["inputs"]:
+            if inp not in (0, self.mask):
+                dot.add_node(pydot.Node(str(inp), label=self.f(inp), ))
+
+        by_tt = {gate["truth_table"]: gate for gate in tcsol["gates"]}
+
+        done = set()
+        queue = list(tcsol["outputs"])
+        while queue:
+            current = queue.pop()
+            if current in done:
+                continue
+            done.add(current)
+            if current in tcsol["inputs"]:
+                c = "#90EE90"
+                if current in tcsol["outputs"]:
+                    c = "#9090EE"
+                dot.add_node(pydot.Node(str(current), label=f'Input:\n{current:016b}', fillcolor=c, style="filled"))
+            else:
+                gate = by_tt[current]
+                gt = gate["gate_type"]
+                if current in tcsol["outputs"]:
+                    c = "#9090EE"
+                elif gt in ("nand", "not"):
+                    c = "#EE9090"
+                else:
+                    c = "#FFFFFF"
+                dot.add_node(pydot.Node(str(current), label=f'{gt}:\n{current:016b}', fillcolor=c, style="filled"))
+                for source in gate["sources"]:
+                    dot.add_edge(pydot.Edge(str(source), str(current)))
+                    if source not in done: queue.append(source)
+        return dot
+
 
 
 def validate(tcsol):
+    if not 2 <= tcsol["input_count"] <= 6:
+        raise ValidationError(f"Expected Input count to be between 2 and 6, got {tcsol['input_count']}")
     validator = Validator(tcsol["input_count"])
-    validator.validate(tcsol["gates"], tcsol["inputs"])
+    validator.validate(validator.sort(tcsol["gates"], tcsol["inputs"]), tcsol["inputs"])
+    validator.to_pydot(tcsol).write_png("test.png")
 
 
-validate(json.load(open("example.tcsol.json")))
+validate(json.load(open("other.tcsol.json")))
